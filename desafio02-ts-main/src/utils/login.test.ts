ed1c2f3cb1681eada21acce6beefd168388efda1
Gabriel Sakura <73794925+gabsakura@users.noreply.github.com>
2024-06-13 00:28:35 -0300

    Add files via upload

---
+++ desafio02-ts-main/src/utils/login.test.ts
@@ -0,0 +1,12 @@
+// src/utils/login.test.ts
+import { welcome } from './welcome';
+
+describe('welcome', () => {
+  const mockAlert = jest.fn();
+  window.alert = mockAlert;
+
+  it('Deve exibir um alert com boas vindas', () => {
+    welcome();
+    expect(mockAlert).toHaveBeenCalledWith('Bem vinda!');
+  });
+});
